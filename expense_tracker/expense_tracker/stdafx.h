a247bc3188bd80066f60d7df7e186162e842b97e
mchesanovskyy <nick.chesanovskyy@gmail.com>
2021-07-19 15:05:26 +0300

    Fixed precompiled headers impl

--- expense_tracker/expense_tracker/stdafx.h
+++ expense_tracker/expense_tracker/stdafx.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <string>
 #include <vector>
+
 #include "./Common/Helpers/str_helpers.h"
 #include "./Common/Helpers/date_time_helpers.h"
 #include "./Common/Helpers/hash_helper.h"
 
 using namespace std;
+using namespace str_helpers;
+using namespace date_time_helpers;
+
 
